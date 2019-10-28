f0fae34598bd8a15cf1f4e1f7c766ea267329a2f
Student <example@example.com>
2019-10-28 16:14:39 -0700

    change double array to single object

--- js/scripts.js
+++ js/scripts.js
@@ -14,44 +14,17 @@ $(document).ready(function() {
     var words = input.map(function(word) {
       return word.toLowerCase();
     });
-    words.sort();
 
-    var wordCounts = [1];
-    console.log(words);
+    var wordlist = {};
 
-    for (var index = 0; index < words.length; index += 1) {
-      for (var nextIndex = (index + 1); index < words.length; nextIndex += 1) {
-        if (words[index] === words[nextIndex]) {
-          wordCounts[index] += 1;
-          words.splice(nextIndex, 1);
-        }
-        else if (index < (words.length - 1)) {
-          wordCounts.push(1);
-          $("ul#word-list").append("<li>" + words[index] + ": " + wordCounts[index] + "</li>");
-          break;
-        } else {
-          $("ul#word-list").append("<li>" + words[index] + ": " + wordCounts[index] + "</li>");
-          break;
-        }
+    words.forEach(function(word) {
+      if (wordlist[word] == null) {
+        wordlist[word] = 1;
+      } else {
+        wordlist[word] += 1;
       }
-    }
-    console.log(words);
-    console.log(wordCounts);
+    });
 
-
-
-
-
-
-    // var sentence = $("#quote").val().split(" ");
-    // var newSentence = sentence.filter(function (word) {
-    //   return word.length > 2;
-    // });
-    // console.log(newSentence);
-    //
-    // newSentence.reverse().join(" ");
-    // console.log(newSentence);
-    //
-
+    console.log(wordlist);
     });
 });
